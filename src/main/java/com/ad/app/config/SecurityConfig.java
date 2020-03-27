ff11e3cc1ac7b7fc1fcb57d6d182c827387560f8
Adeeb Siddiqui <adeeb.bd@gmail.com>
2020-03-27 03:08:03 -0500

    Added comment how to see logout message

--- src/main/java/com/ad/app/config/SecurityConfig.java
+++ src/main/java/com/ad/app/config/SecurityConfig.java
@@ -11,7 +11,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
+            .csrf().disable()//comment out to see the logout message
             .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
